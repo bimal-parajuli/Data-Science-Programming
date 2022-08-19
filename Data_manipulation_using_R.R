3c4a7b14e0ce79e2551c3d935ea0c6c4a8916558
Bimal parajuli <65300475+bimal-parajuli@users.noreply.github.com>
2022-08-19 12:48:35 +0530

    Add Data manipulation Problems

---
+++ Data_manipulation_using_R.R
@@ -0,0 +1,87 @@
+# 1.
+# Create user defined functions to perform various arithmetic operations 
+# and call each functions using a menu driven program.
+# Try functions with parameters and without parameters.
+
+
+
+
+# 2. 
+# Perform basic statistical operations on a random vector of 100 elements.
+
+# a. Mean
+
+# b. Median
+
+# c. Mode
+
+# d. Range
+
+# e. IQR
+
+# f. Standard Deviation
+
+# g. Summary
+
+# h. Histogram
+
+# i. Table
+
+
+
+
+3.
+# Data Frame operations
+
+# a. Create a Data Frame
+
+# b. Access a Component ([, [[, $)
+
+# c. Structure of Data Frame
+
+# d. Add a new Column
+
+# e. Add a new row.
+
+# f. Delete Column
+
+# g. Delete Specific row
+
+# h. Order data frame (with, order, arrange)
+
+
+
+
+# 4. 
+
+# Read Air Quality dataset and handle the handle the missing data using following technique
+
+# a. Drop row
+
+# b. Drop Column
+
+# c. Imputation (Replace with unknown, mean or Group mean)
+
+
+
+
+# 5. Perform label Encoding on IRIS dataset
+
+
+
+
+# 6. Perform one-hot encoding on dataset
+
+
+
+
+# 7. Feature Scaling or standardization
+
+# a. Normalization.
+
+# b. Z-Scale.
+
+
+
+
+
